99a32e57fa54c4131f2928de6174a634b499934c
Rubi Quintas Alonso <untalrubi@gmail.com>
2024-12-01 16:35:58 +0100

    feat(RANDOM PERSONAJE): Crear carta jugador
    Creada una carta en la interfaz que tomará la imagen del jugador.

--- quien_es_quien/quien_es_quien.py
+++ quien_es_quien/quien_es_quien.py
@@ -1,39 +1,41 @@
 import reflex as rx
-
 from rxconfig import config
 
 
 def cabecera() -> rx.Component:
-    return rx.heading(
-        rx.container(
-            rx.heading("¿Quién es Quién?", as_="h1", size="8"),
-        )
-)
+    return rx.heading("¿Quién es Quién?", as_="h1", size="8", margin="1em")
 
 
 def tablero() -> rx.Component:
     #Tablero personajes
-    return rx.box(
-        rx.grid(
-            rx.foreach(
-                rx.Var.range(24),
-                lambda i: rx.card(f"Personaje {i + 1}", height="13em", width="7.5em"),
-            ),
-                columns="8",
-                rows="3",
-                spacing="3",
-        )
+    return rx.grid(
+        rx.foreach(
+            rx.Var.range(24),
+            lambda i: rx.card(f"Personaje {i + 1}", height="13em", width="7.5em"),
+        ),
+            columns="8",
+            rows="3",
+            spacing="3",
     )
+
+
+def jugador() -> rx.Component:
+    #Carta jugador
+    return rx.card("Jugador", height="17em", width="10em", margin_left="5em")
+
 
 def index() -> rx.Component:
     #Página inicial
-    return rx.box(
-        rx.vstack(
-            cabecera(),
+    return rx.vstack(
+        cabecera(),
+        rx.hstack(
             tablero(),
+            jugador(),
             align="center",
-        )
+        ), 
+        align="center"
     )
+
 
 app = rx.App()
 app.add_page(index)
