c9bc53367f6a8d2597636aec6fe9c89d51ea3f3d
avedado <mariaalonsoteis@gmail.com>
2024-12-02 14:30:52 +0100

    feat(CARACTERISTICAS): Diferenciar caracteristicas
    Diferenciar nariz y boca del resto de caracteristicas

--- quien_es_quien/caracteristicas.py
+++ quien_es_quien/caracteristicas.py
@@ -1,20 +1,25 @@
-
 def extraer_palabras_clave(pregunta): 
-    caracteristicas = ['hombre', 'mujer', 'barba', 'pelirrojo', 'marrones', 'orejas', 'grandes', 'mofletes', 'mejillas', 'sonrosadas', 'calva', 'pequeña', 'nariz', 'pequeña', 
-                      'bigote', 'ojos', 'azules', 'largo', 'raya', 'medio', 'pelo', 'negro', 'boca', 'grande', 'labios', 'gruesos', 'corto', 'gafas', 'alargada', 'blanco', 
-                      'canas', 'cejas', 'gruesas', 'lado', 'sombrero', 'pendientes', 'castaño', 'finas', 'rubio', 'cara', 'triste', 'gorra'] 
+    caracteristicas = ['hombre', 'mujer', 'barba', 'pelirrojo', 'marrones', 'orejas', 'grandes', 'mofletes', 'mejillas', 'sonrosadas', 'calva', 'pelirroja',
+                      'bigote', 'ojos', 'azules', 'largo', 'raya', 'medio', 'pelo', 'negro', 'labios', 'gruesos', 'corto', 'gafas', 'alargada', 'blanco', 'canas', 
+                      'cejas', 'gruesas', 'lado', 'sombrero', 'pendientes', 'castaño', 'finas', 'rubio', 'cara', 'triste', 'gorra', 'finos','calvo','marrón', 'rubia'] 
+    forma_nariz=['grande', 'pequeña']
+    lista=['boca','nariz']
     pregunta = pregunta.lower() 
     caracteres_especiales = "?¿:;,.¡!" 
     for caracter in caracteres_especiales: 
         pregunta = pregunta.replace(caracter, "") 
-    resultado = set()
-    palabras = pregunta.split() 
-    for palabra_clave in caracteristicas: 
-        if palabra_clave in palabras: 
-            resultado.add(palabra_clave) 
-    if not resultado: 
+    respuesta = []
+    for palabra_clave in pregunta: 
+        if palabra_clave in lista:
+            respuesta.append(palabra_clave)
+            if palabra_clave in forma_nariz: 
+                respuesta.append(palabra_clave) 
+                return respuesta
+        if palabra_clave in caracteristicas:
+                respuesta.append(palabra_clave) 
+    if not respuesta: 
         return f"No se encontró la característica. Intenta con las siguientes características posibles: {', '.join(caracteristicas)}" 
-    return resultado
+    return respuesta
 
 
 pregunta = "¿Tiene los ojos azules?"
@@ -22,8 +27,14 @@ print(extraer_palabras_clave(pregunta))
 pregunta = "¿Tiene la cara triste?"
 print(extraer_palabras_clave(pregunta))
 pregunta = "¿Está triste?"
-print(extraer_palabras_clave(pregunta)) #####
+print(extraer_palabras_clave(pregunta)) ####
 pregunta = "Tiene los ojos verdes?"
 print(extraer_palabras_clave(pregunta))
 pregunta = "Tiene hoyuelos?"
+print(extraer_palabras_clave(pregunta))
+pregunta = "Tiene cara?"
+print(extraer_palabras_clave(pregunta)) ####
+pregunta = "Tiene labios finos?"
+print(extraer_palabras_clave(pregunta))
+pregunta = "Tiene la boca grande?"
 print(extraer_palabras_clave(pregunta))
